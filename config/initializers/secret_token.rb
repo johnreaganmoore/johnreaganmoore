d2a79711906196e218e248673d6ae79f8ad7a5b4
John Reagan Moore <johnreaganmoore@Johns-MacBook-Pro.local>
2014-02-01 18:33:29 -0600

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Johnreaganmoore::Application.config.secret_key_base = '13df0f58408d8bbb42234318838603d44ffa613b30bd2bfa25b8f01ba8e7ae0925ef8a0fbd2c627fb21e0e350578544b5dbda7720eac272f37d1f56ed59876fb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Johnreaganmoore::Application.config.session_store :cookie_store, key: '_johnreaganmoore_session'
