44ecc25f62e6454d054a7a2a3e3e40975a7fcd10
johnreaganmoore <johnreaganmoore@gmail.com>
2014-10-08 10:15:30 -0400

    Adds header layout

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140917152224) do
+ActiveRecord::Schema.define(version: 20140921014503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,11 @@ ActiveRecord::Schema.define(version: 20140917152224) do
     t.datetime "updated_at"
     t.string   "author"
     t.string   "publish_date"
+  end
+
+  create_table "users", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
